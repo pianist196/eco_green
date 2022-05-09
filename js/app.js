ec0447ac5c80c6effd41d53dfa7bc577dd395a4b
Alexey <1234569050@rambler.ru>
2022-05-09 09:26:17 +0500

    rt

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-console.log('heelo');
+new Swiper('.main__slider', {
+    autoHeight: true,
+    slidesPerView: 2.2
+})
+
+new Swiper('.plant__slider', {
+    autoHeight: true,
+    slidesPerView: 1.2,
+    spaceBetween: 20,
+})
